d0b9f20d1325d045baec99fa2e395624be7a3df5
Brandyn Bold <brandynbold@gmail.com>
2015-09-01 16:49:05 -0400

    Add extended js and observer code

--- app/code/community/SomethingDigital/AjaxAddtoCart/Model/Observer.php
+++ app/code/community/SomethingDigital/AjaxAddtoCart/Model/Observer.php
@@ -4,6 +4,40 @@ class SomethingDigital_AjaxAddtoCart_Model_Observer
 {
   public function ajaxAction(Varien_Event_Observer $observer)
   {
-    
+    if(!$observer->getControllerAction()->getRequest()->isAjax()) {
+      return;
+    }
+
+    $catalogModel = Mage::getModel('catalog/product');
+    $storeId      = Mage::app()->getStore()->getId();
+    $productId    = $observer->getControllerAction()->getRequest()->getParam('product');
+    $product      = $catalogModel->setStoreId($storeId)->load($productId);
+
+    $repsonse     = array();
+
+    try {
+      if (!$this->_getSession()->getNoCartRedirect(true)) {
+
+        if (!$product) {
+          $response['status'] = 'ERROR';
+          $response['message'] = $this->__('Unable to find Product ID');
+        }
+
+        $message = $this->__('%s was added to your shopping cart.', Mage::helper('core')->htmlEscape($product->getName()));
+        $response['status'] = 'SUCCESS';
+        $response['message'] = $message;
+        $this->loadLayout();
+        $sidebar = $this->getLayout()->getBlock('minicart_head')->toHtml();
+        $response['minicart_head'] = '<div class="header-minicart">' . $sidebar . '</div>';
+      }
+    } catch(Mage_Core_Exception $e) {
+          $response['status'] = 'ERROR';
+          $response['message'] = $this->__('Cannot add the item to shopping cart.');
+          Mage::logException($e);
+    }
+    if($response['status'] == 'ERROR'){
+        $response['message'] = '<ul class="messages"><li class="error-msg"><ul><li class="out-of-stock-error">' . $response['message'] . '</li></ul></li></ul>';
+    }            
+    $observer->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
   }
 }
--- skin/frontend/base/default/js/somethingdigital/ajaxaddtocart.js
+++ skin/frontend/base/default/js/somethingdigital/ajaxaddtocart.js
@@ -1,19 +1,69 @@
-'use strict';
-var AjaxAddtoCart = Class.create();
+// 'use strict';
 
-AjaxAddtoCart.prototype = {
-  initialize: function() {
-    this.createLoadingModal();
-  },
-  createLoadingModal: function() {
-    let loadingModal = new Element('div', {id: 'loading-modal', class: 'loading-modal'});
-    document.body.appendChild(loadingModal);
-  },
-  showLoadingModal: function() {
+productAddToCartForm.submit = productAddToCartForm.submit.wrap(function(button, url){
+  if (this.validator.validate()) {
+    $j('body').addClass('locked');
+    loadingModal.show();
+    var form = this.form;
+    var oldUrl = form.action;
+    var e = null;
 
+    if (url) {
+      form.action = url;
+    }
+
+        console.log($j('#product_addtocart_form').serialize());////////////////////////////////////////////////////////////////////////////////////////// console.log Right Here ////////////////////////////////
+
+    try {
+      $j.ajax({
+        url: form.action,
+        dataType: 'json',
+        type : 'post',
+        data: $j('#product_addtocart_form').serialize()
+      })
+      .done(function(data){
+        $j('body').removeClass('locked');
+        $j('html,body').animate({scrollTop: 0}, 250);
+        if (data.status === "SUCCESS") {
+          $updatedCart = $j(data.minicart_head);
+          $j('#header-cart').html($updatedCart.find('#header-cart').html());
+          $j('.skip-cart').html($updatedCart.find('.skip-cart').html()).trigger('click'); 
+
+        } else if (data.status === "ERROR") {
+          $j('.col-main').prepend(data.message);
+          $j('.col-main .messages').delay(5000).fadeOut(function(){$j(this).remove();});
+        }
+      })
+      .always(function(){loadingModal.remove();})
+      .fail(function(){loadingModal.remove();});
+    } catch (e) {
+    }
+    this.form.action = oldUrl;
+    if (e) {
+      throw e;
+    }
   }
-}
+  }.bind(productAddToCartForm));
 
-Event.observe(document, 'dom:loaded', function() {
-    new AjaxAddtoCart();
-});
+// var AjaxAddtoCart = Class.create();
+// AjaxAddtoCart.prototype = {
+
+//   initialize: function() {
+//     this.createLoadingModal();
+//     console.log('bada boom');////////////////////////////////////////////////////////////////////////////////////////// console.log Right Here ////////////////////////////////
+//   },
+//   submitAddtoCart: function() {
+    
+//   },
+//   createLoadingModal: function() {
+//     let loadingModal = new Element('div', {id: 'loading-modal', class: 'loading-modal'});
+//     document.body.appendChild(loadingModal);
+//   },
+//   showLoadingModal: function() {
+
+//   }
+// }
+
+// Event.observe(document, 'dom:loaded', function() {
+//     new AjaxAddtoCart();
+// });
