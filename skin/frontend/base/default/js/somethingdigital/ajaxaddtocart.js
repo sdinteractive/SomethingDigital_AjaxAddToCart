9f86d5d51fdcf0f2459fa02d2f98c997cf23f3cf
Phillip Jackson <philwinkle@gmail.com>
2015-09-02 10:36:08 -0400

    Refactor the js

--- skin/frontend/base/default/js/somethingdigital/ajaxaddtocart.js
+++ skin/frontend/base/default/js/somethingdigital/ajaxaddtocart.js
@@ -1,49 +1,55 @@
 'use strict';
 
-var loadingModal = $j('<div class="loadingModal"/>');
-$j('body').append(loadingModal);
+(function($, loadingModal, productAddToCartForm){
 
-productAddToCartForm.submit = productAddToCartForm.submit.wrap(function(button, url){
-  if (this.validator.validate()) {
-    $j('body').addClass('locked');
-    loadingModal.show();
-    var form = this.form;
-    var oldUrl = form.action;
-    var e = null;
+  $body = $('body');
 
-    if (url) {
-      form.action = url;
+  productAddToCartForm.submit = productAddToCartForm.submit.wrap(function(button, url){
+    if (this.validator.validate()) {
+      $body.addClass('locked');
+      loadingModal.show();
+      var form = this.form,
+          oldUrl = form.action,
+          e = null;
+
+      if (url) {
+        form.action = url;
+      }
+
+      try {
+        $.ajax({
+          url: form.action,
+          dataType: 'json',
+          type : 'post',
+          data: $('#product_addtocart_form').serialize()
+        })
+        .done(function(data){
+          $body.removeClass('locked');
+          var $updatedCart = $(data.minicart_head);
+
+          //TODO: make animation scroll optional
+          $('html,body').animate({scrollTop: 0}, 250);
+
+          //apply the minicart update and unfurl it
+          $('#header-cart').html($updatedCart.find('#header-cart').html());
+          $('.skip-cart').html($updatedCart.find('.skip-cart').html()).trigger('click'); 
+        })
+        .fail(function(data){
+          //display failure message
+          $('.col-main').prepend(data.message);
+          $('.col-main .messages').delay(5000).fadeOut(function(){$(this).remove();});
+          loadingModal.remove()
+        })
+        .always(loadingModal.remove);
+      } catch (e) {
+        console.error(e);
+      }
+      this.form.action = oldUrl;
+      if (e) {
+        throw e;
+      }
     }
-
-        console.log($j('#product_addtocart_form').serialize());////////////////////////////////////////////////////////////////////////////////////////// console.log Right Here ////////////////////////////////
-
-    try {
-      $j.ajax({
-        url: form.action,
-        dataType: 'json',
-        type : 'post',
-        data: $j('#product_addtocart_form').serialize()
-      })
-      .done(function(data){
-        $j('body').removeClass('locked');
-        $j('html,body').animate({scrollTop: 0}, 250);
-        if (data.status === "SUCCESS") {
-          var $updatedCart = $j(data.minicart_head);
-          $j('#header-cart').html($updatedCart.find('#header-cart').html());
-          $j('.skip-cart').html($updatedCart.find('.skip-cart').html()).trigger('click'); 
-
-        } else if (data.status === "ERROR") {
-          $j('.col-main').prepend(data.message);
-          $j('.col-main .messages').delay(5000).fadeOut(function(){$j(this).remove();});
-        }
-      })
-      .always(function(){loadingModal.remove();})
-      .fail(function(){loadingModal.remove();});
-    } catch (e) {
-    }
-    this.form.action = oldUrl;
-    if (e) {
-      throw e;
-    }
-  }
   }.bind(productAddToCartForm));
+
+
+})(jQuery, loadingModal, productAddToCartForm);
