f3b927d1ca9e94e4934f0225c152ec5360bf6bc9
Robert Nicklin <rnicklin@users.noreply.github.com>
2015-10-07 17:21:20 -0400

    Wrap sdAjaxaddtocart in closure, allowing dollar sign jQuery alias

--- skin/frontend/base/default/js/somethingdigital/ajaxaddtocart/ajaxaddtocart.js
+++ skin/frontend/base/default/js/somethingdigital/ajaxaddtocart/ajaxaddtocart.js
@@ -1,85 +1,85 @@
 'use strict';
 
-window.sdAjaxaddtocart = {
-    init:function(productAddToCartForm) {
-        var $body = jQuery('body');
-
-        productAddToCartForm.submit = productAddToCartForm.submit.wrap(function(button, url){
-            var form   = this.form;
-            var oldUrl = form.action;
-            var e      = null;
-
-            if (this.validator.validate()) {
-                $body.addClass('locked');
-                loadingModal.show();
-
-                try {
-                    jQuery.ajax({
-                        url: form.action,
-                        dataType: 'json',
-                        type: 'post',
-                        data: jQuery('#product_addtocart_form').serialize()
-                    })
-                        .done(function(data){
-                            var $updatedCart   = jQuery(data.minicart_head);
-                            var headerCartHtml = $updatedCart.find('#header-cart').html();
-                            var skipCartHtml   = $updatedCart.find('.skip-cart').html();
-
-                            $body.removeClass('locked');
-
-                            // If add to cart from quickview, close quickview
-                            if(typeof(window.sdQuickview.close) == "function"
-                                && jQuery('#sd-quickview').is(':visible')) {
-                                window.sdQuickview.close();
-                            }
-
-                            //TODO: make animation scroll optional
-                            jQuery('html,body').animate({scrollTop: 0}, 250);
-
-                            //apply the minicart update and unfurl it
-                            jQuery('#header-cart').html(headerCartHtml);
-                            jQuery('.skip-cart').html(skipCartHtml).trigger('click');
-                        })
-                        .fail(function(data){
-                            // display failure message
-                            // if add to cart from quickview
-                            if(typeof(window.sdQuickView) == "object" && jQuery('#sd-quickview').is(':visible')) {
-                                window.sdQuickview.content.prepend(data.message);
-
-                                //remove the failure message after 5s
-                                window.sdQuickview.content.find('.messages')
-                                    .delay(5000)
-                                    .fadeOut(function(){
-                                        jQuery(this).remove();
-                                    });
-                            } else {
-                                jQuery('.col-main').prepend(data.message);
-
-                                //remove the failure message after 5s
-                                jQuery('.col-main .messages')
-                                    .delay(5000)
-                                    .fadeOut(function(){
-                                        jQuery(this).remove();
-                                    });
-                            }
-
-                            //unset the modal block
-                            loadingModal.remove();
-                        })
-                        .always(loadingModal.remove);
-                } catch (e) {
-                    console.error(e);
-                }
-                this.form.action = oldUrl;
-                if (e) {
-                    throw e;
-                }
-            }
-        }.bind(productAddToCartForm));
-    }
-};
-
 (function($){
+
+    window.sdAjaxaddtocart = {
+        init:function(productAddToCartForm) {
+            var $body = $('body');
+
+            productAddToCartForm.submit = productAddToCartForm.submit.wrap(function(button, url){
+                var form   = this.form;
+                var oldUrl = form.action;
+                var e      = null;
+
+                if (this.validator.validate()) {
+                    $body.addClass('locked');
+                    loadingModal.show();
+
+                    try {
+                        $.ajax({
+                            url: form.action,
+                            dataType: 'json',
+                            type: 'post',
+                            data: $('#product_addtocart_form').serialize()
+                        })
+                            .done(function(data){
+                                var $updatedCart   = $(data.minicart_head);
+                                var headerCartHtml = $updatedCart.find('#header-cart').html();
+                                var skipCartHtml   = $updatedCart.find('.skip-cart').html();
+
+                                $body.removeClass('locked');
+
+                                // If add to cart from quickview, close quickview
+                                if(typeof(window.sdQuickview.close) == "function"
+                                    && $('#sd-quickview').is(':visible')) {
+                                    window.sdQuickview.close();
+                                }
+
+                                //TODO: make animation scroll optional
+                                $('html,body').animate({scrollTop: 0}, 250);
+
+                                //apply the minicart update and unfurl it
+                                $('#header-cart').html(headerCartHtml);
+                                $('.skip-cart').html(skipCartHtml).trigger('click');
+                            })
+                            .fail(function(data){
+                                // display failure message
+                                // if add to cart from quickview
+                                if(typeof(window.sdQuickView) == "object" && $('#sd-quickview').is(':visible')) {
+                                    window.sdQuickview.content.prepend(data.message);
+
+                                    //remove the failure message after 5s
+                                    window.sdQuickview.content.find('.messages')
+                                        .delay(5000)
+                                        .fadeOut(function(){
+                                            $(this).remove();
+                                        });
+                                } else {
+                                    $('.col-main').prepend(data.message);
+
+                                    //remove the failure message after 5s
+                                    $('.col-main .messages')
+                                        .delay(5000)
+                                        .fadeOut(function(){
+                                            $(this).remove();
+                                        });
+                                }
+
+                                //unset the modal block
+                                loadingModal.remove();
+                            })
+                            .always(loadingModal.remove);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                    this.form.action = oldUrl;
+                    if (e) {
+                        throw e;
+                    }
+                }
+            }.bind(productAddToCartForm));
+        }
+    };
 
     if(typeof(productAddToCartForm) != "undefined") {
         sdAjaxaddtocart.init(productAddToCartForm);
